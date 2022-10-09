da1223681b0556c26fee69016501965bb6de39df
Vivek Velineni <vivekvelineni@gmail.com>
2022-10-10 02:52:19 +0530

    testing whole website by using AXIOS

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,56 +1,121 @@
-function Find() {
-    var v = 0
-    var Rawinput = document.forms["form"]["input_link"].value;
-    var input = Rawinput.trim();
-      if (/\s/.test(input) != true) {
-  
-        if ((input.length == 11) && (/[/=&?.:%]+/.exec(input) == null)){
-          //console.log('it is a video id');
-          return VideoOP()
-        } else {
-              v = v + 1
-          }
-  
-        if ((input.includes('youtu') == true) && (input.includes('list') != true) && (input.length >= 20)){
-          //console.log('it is a video link');
-          return VideoOP()
-        } else {
-              v = v + 1
-          }
-  
-        if (((input.length == 34) || (input.length == 13)) && (/[/=&?.:%]+/.exec(input) == null)){
-          //console.log('it is a playlist id');
-          return VideoOP()
-        } else {
-              v = v + 1
-          }
-  
-        if ((input.includes('list') == true) && (input.includes('youtu') == true)){
-          //console.log('it is a playlist link');
-          return VideoOP()
-        } else {
-              v = v + 1
-          }
-  
-      } else { 
-          return Error()
-        }
-  
-        if (v == 4) {
-          return Error()
-        }
-  
-        function Error() {
-          document.getElementById("gif").setAttribute("style", "display : none;");
-          document.getElementById("errorid").setAttribute("style", "display : block;");
-          document.getElementById("op").setAttribute("style", "display : none;");
-          document.getElementById("vop").setAttribute("style", "display : none;");
-          document.getElementById("lop").setAttribute("style", "display : none;");
-        }
+<!DOCTYPE html>
+<html lang = 'en'>
+
+<head>
+    <title>YouTube Playlist & Video Duration Finder</title>
+    <meta charset="UTF-8" />
+    <script type="module" src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <link rel = "shortcut icon" href = "assets/images/playlisticon.ico">
+    <link rel = "stylesheet"  type = "text/css" href = "assets/css/index.css"/>
+    <link rel="stylesheet" type="text/css" media="only screen and (max-device-width: 600px)" href="assets/css/mobile.css" />
+    <script src = "assets/js/apirequest.js"></script>
+</head>
+
+<body>
         
-    function VideoOP() { 
-          document.getElementById("op").setAttribute("style", "display : none;");
-          document.getElementById("vop").setAttribute("style", "display : block;");
-          document.getElementById("lop").setAttribute("style", "display : none;");
-    }
-}
+    <noscript> <META HTTP-EQUIV="Refresh" CONTENT="0; URL = JSError.html"> </noscript>
+
+
+    <div class = 'padleft'>
+        <a onClick="window.location.reload()">
+            <img class = 'logoimg' src = 'assets/images/playlistlogo.svg'>
+        </a>
+    </div>
+
+    <div class = 'padright'>
+        <a href = 'https://github.com/vivekvelineni' target = '_blank' title = "Vivek Velineni's github">
+            <img class = 'gitimg' src = 'assets/images/githubicon.svg'>
+        </a>
+    </div>
+
+    <h1 class = 'navbar'><FONT COLOR = 'black'>YouTube</FONT> Playlist & Video Duration 
+        <a onClick="window.location.reload()" class = 'nodec'><FONT COLOR = 'blue'><u>Finder</u></FONT>🔎</a></h1>
+    
+        <hr class = 'horizontal' noshade>
+    
+
+    <h3 name = 'input_name' class = 'maintext'>Paste your YouTube
+        Playlist or Video URL/ID below and click 'Find'</h3>
+    
+    <form name = 'form' method = 'post'>
+        <input class = 'font lh' type = 'text' name = 'input_link' 
+            size = '110' placeholder = 'Paste your YouTube Playlist URL here' required/>   
+    </form>
+
+    <br height="40%">
+        <input type = 'submit' id = 'button' class = 'font lhb' value = 'Find🔎' onclick = "Find()">
+    <br>
+
+    <div class = 'scroll'>Scroll down the box to check for more info.</div>
+
+    <br>
+    
+    <div class = 'divgif gif' id = 'gif'>
+
+        <img src = "assets\images\winnie-the-pooh.gif"
+        class = 'gif gifpad' style = 'display : block;'>
+
+    </div> 
+
+    <div class = 'op font' id = 'op' style = 'display : none;'>
+        <b>
+        <p id = 'Ptitle' class = 'rightpad color titlepad'></p>
+        <p id = 'Pchan' class = 'rightpad color'></p>
+        <p id = 'Ptdur' class = 'rightpad color'></p>
+        <p id = 'Padur' class = 'rightpad color'></p>
+        <p id = 'Ptvid' class = 'rightpad color'></p>
+        <p id = 'Pdur0.5' class = 'rightpad color'></p>
+        <p id = 'Pdur0.75' class = 'rightpad color'></p>
+        <p id = 'Pdur1.25' class = 'rightpad color'></p>
+        <p id = 'Pdur1.5' class = 'rightpad color'></p>
+        <p id = 'Pdur1.75' class = 'rightpad color'></p>
+        <p id = 'Pdur2' class = 'rightpad color'></p>
+        </b>
+    </div>
+
+    <div id = 'vop' style = 'display : none;'>
+        <div class = 'vop font' >
+        <b>
+            <p id = 'Vtitle' class = 'rightpad titlepad color'></p>
+            <p id = 'Vchan' class = 'rightpad color'></p>
+            <p id = 'Vtdur' class = 'rightpad color'></p>
+            <p id = 'Vdur0.5' class = 'rightpad color'></p>
+            <p id = 'Vdur0.75' class = 'rightpad color'></p>
+            <p id = 'Vdur1.25' class = 'rightpad color'></p>
+            <p id = 'Vdur1.5' class = 'rightpad color'></p>
+            <p id = 'Vdur1.75' class = 'rightpad color'></p>
+            <p id = 'Vdur2' class = 'rightpad color'></p>
+        </b>
+        </div>
+        <br>
+    </div>
+
+    <div id = 'errorid' class = "error" style = 'display : none;'>
+    
+        <p class = 'rightpad titlepad'>Well 👉👈, unfortunately there seems to be some <FONT COLOR = 'red'>problem</FONT> 
+            with the link that you have provided or <FONT COLOR = 'red'> <br>You might not have provided any link at all.</FONT></p>
+        <p class = 'rightpad'>Some <FONT COLOR = 'red'>errors</FONT> with the link might be:</p>
+        <p class = 'rightpad'>1. Your link might be <FONT COLOR = 'red'>expired/invalid/incomplete</FONT>.</p>
+        <p class = 'rightpad'>2. You might have pasted a <FONT COLOR = 'red'>shortened URL.</FONT> Please paste original URL.</p>
+        <p class = 'rightpad'>3. Correct Video link looks like this: <FONT COLOR = 'red'>www.youtube.com/watch?v=YQHsXMglC9A</FONT></p>
+        <p class = 'rightpad'>4. Correct Playlist link: <FONT COLOR = 'red'>https://youtube.com/playlist?list=PL8dPuuaLjXtNlUrzyH5r6jN9ulIgZBpdo</FONT></p>
+        <p class = 'rightpad'style = 'text-align : center;'><FONT COLOR = 'red'>Please check your input link with correct link and try again.</FONT></p>
+    
+    </div>
+
+    <div id = 'lop' class = "lop" style = 'display : none;'>
+        <br>
+        <p class = 'rightpad'style = 'text-align : center;'><FONT COLOR = 'red'>It's a Live Video.</FONT></p>
+    
+    </div>
+
+</body>
+
+<footer class = 'footer font'>
+    <p>For huge playlists, usual loading time is 5 Sec or more. Please wait patiently.</p>
+    <p>Made with 💖 by
+        <a class = 'nodec footrcolour' href = 'https://github.com/vivekvelineni' 
+        target = '_blank' title = "Vivek Velineni's github"> Vivek Velineni</a></p>
+</footer>
+
+</html>
